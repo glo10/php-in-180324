17cef6ee9b34e8aa55deb91b850ddf3a92fd4c2f
formateur-glodiee-tshimini <contact@tshimini.fr>
2024-03-20 11:28:05 +0100

    feat: demo form

---
+++ demo/form/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Demo formulaire</title>
+</head>
+<body>
+  <form action="submit.php">
+    <input
+      type="text"
+      name="lastname"
+      placeholder="Votre nom">
+    <input type="text" name="firstname"
+      placeholder="Votre prénom">
+    <input type="submit" value="Valider">
+  </form>
+</body>
+</html>
