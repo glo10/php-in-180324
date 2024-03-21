4f372295a37e5f7afc99f575e76a8e21e0c68426
formateur-glodiee-tshimini <contact@tshimini.fr>
2024-03-21 11:23:52 +0100

    refactor: update

--- demo/pdo/index.php
+++ demo/pdo/index.php
@@ -1,6 +1,5 @@
 <?php
 
-require_once dirname(__DIR__).'/../1-exercices/corrections/ex10/back/classes/User.php';
 try {
   $requestSQL = 'SELECT id, name FROM country LIMIT 5';
   $db = new PDO('mysql:host=localhost;port=3308;dbname=mvc-rss','root','');
@@ -10,10 +9,11 @@ try {
   //   var_dump($row);
   // }
   $pdoStatement = $db->prepare($requestSQL);
-  $pdoStatement->execute();
-  // avec fetchAll et les options sur le format du résultat
-  $resultsFetchAll = $pdoStatement->fetchAll(PDO::FETCH_CLASS, "user");
-  var_dump($resultsFetchAll);
+  if($pdoStatement->execute()) { 
+    // avec fetchAll et les options sur le format du résultat
+    $resultsFetchAll = $pdoStatement->fetchAll();
+    var_dump($resultsFetchAll);
+  }
 } catch(PDOException $e) {
   echo 'erreur PDO '.$e->getMessage();
 }
